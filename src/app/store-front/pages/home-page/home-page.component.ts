73efde8f4e53528173534e16aab73f94140a3a2e
Esther Manrique <esthermanriqueepam@gmail.com>
2025-11-09 11:35:36 -0600

    Add pagination component and service, integrate into home page

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -3,30 +3,35 @@ import { rxResource } from '@angular/core/rxjs-interop';
 
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { 
-
+export class HomePageComponent {
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  // activatedRoute = inject(ActivatedRoute);
+
+  // currentPage = toSignal(
+  //   this.activatedRoute.queryParamMap.pipe(
+  //     map((params) => (params.get('page') ? +params.get('page')! : 1)),
+  //     map((page) => (isNaN(page) ? 1 : page))
+  //   ),
+  //   { initialValue: 1 }
+  // );
 
   productsResource = rxResource({
-    request: () => ({}),
-    loader: ({ request}) => {
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
+    loader: ({ request }) => {
       return this.productsService.getProducts({
-        // limit: 1,
-        // offset: 0,
-        // gender: 'kid',
+        offset: request.page  * 9,
+        // limit: 9,
       });
-    }
-
-  })
-
-
-
-
-
+    },
+  });
 }
