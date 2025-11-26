23725008e3a21ec1fd11bba5f2292cc4b370c512
Esther Manrique <esthermanriqueepam@gmail.com>
2025-11-25 19:49:39 -0600

    Add ProductsTableComponent with product listing and details view

--- src/app/admin-dashboard/admin-dashboard.routes.ts
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -19,7 +19,7 @@ export const adminDashboardRoutes: Routes = [
         component: ProductsAdminPageComponent
       },
       {
-        path: 'product/:id',
+        path: 'products/:id',
         component: ProductAdminPageComponent
       },
       {
