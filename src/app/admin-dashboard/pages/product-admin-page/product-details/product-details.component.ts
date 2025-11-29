7ea29738f116ba5c62c6e5afe4f23dde336c6d0d
Esther Manrique <esthermanriqueepam@gmail.com>
2025-11-28 18:55:28 -0600

    Refactor product details component for improved readability and maintainability

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, input, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
-import { ProductCarouselComponent } from "@products/components/product-carousel/product-carousel.component";
+import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
 import { Product } from '@products/interfaces/product.interface';
 import { FormUtils } from '@utils/form-utils';
 
@@ -10,8 +10,7 @@ import { FormUtils } from '@utils/form-utils';
   imports: [ProductCarouselComponent, ReactiveFormsModule],
   templateUrl: './product-details.component.html',
 })
-export class ProductDetailsComponent implements OnInit { 
-
+export class ProductDetailsComponent implements OnInit {
   product = input.required<Product>();
 
   fb = inject(FormBuilder);
@@ -19,13 +18,19 @@ export class ProductDetailsComponent implements OnInit {
   productForm = this.fb.group({
     title: ['', Validators.required],
     description: ['', Validators.required],
-    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    slug: [
+      '',
+      [Validators.required, Validators.pattern(FormUtils.slugPattern)],
+    ],
     price: [0, [Validators.required, Validators.min(0)]],
     stock: [0, [Validators.required, Validators.min(0)]],
     sizes: [['']],
     images: [[]],
     tags: [''],
-    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
+    gender: [
+      'men',
+      [Validators.required, Validators.pattern(/men|women|kid|unisex/)],
+    ],
   });
 
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
@@ -33,12 +38,24 @@ export class ProductDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.setFormValue(this.product());
   }
-  
-  setFormValue( formLike: Partial<Product>) {
+
+  setFormValue(formLike: Partial<Product>) {
     this.productForm.reset(this.product() as any);
     // this.productForm.patchValue( formLike as any);
 
     this.productForm.patchValue({ tags: formLike.tags?.join(', ') });
+  }
+
+  onSizeClicked(size: string) {
+    const currentSizes = this.productForm.value.sizes ?? [];
+
+    if(currentSizes.includes(size)) {
+      currentSizes.splice(currentSizes.indexOf(size), 1);
+    } else {
+      currentSizes.push(size);
+    }
+    
+    this.productForm.patchValue({ sizes: currentSizes });
   }
 
   onSubmit() {
@@ -48,5 +65,5 @@ export class ProductDetailsComponent implements OnInit {
     // }
 
     console.log(this.productForm.value);
-  } 
+  }
 }
