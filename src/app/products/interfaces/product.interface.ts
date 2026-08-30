a1f1681f4ef4060e250cca88c758d992ef795767
Esther Manrique <105511137+EstherManriqueGlez@users.noreply.github.com>
2026-08-30 14:25:40 -0600

    Refactor code style in product interface and update angular.json

--- src/app/products/interfaces/product.interface.ts
+++ src/app/products/interfaces/product.interface.ts
@@ -1,38 +1,37 @@
-import { User } from "@auth/interfaces/user.interface";
+import { User } from '@auth/interfaces/user.interface';
 
 export interface ProductsResponse {
-  count:    number;
-  pages:    number;
+  count: number;
+  pages: number;
   products: Product[];
 }
 
 export interface Product {
-  id:          string;
-  title:       string;
-  price:       number;
+  id: string;
+  title: string;
+  price: number;
   description: string;
-  slug:        string;
-  stock:       number;
-  sizes:       Size[];
-  gender:      Gender;
-  tags:        string[];
-  images:      string[];
-  user:        User;
+  slug: string;
+  stock: number;
+  sizes: Size[];
+  gender: Gender;
+  tags: string[];
+  images: string[];
+  user: User;
 }
 
 export enum Gender {
-  Kid = "kid",
-  Men = "men",
-  Unisex = "unisex",
-  Women = "women",
+  Kid = 'kid',
+  Men = 'men',
+  Unisex = 'unisex',
+  Women = 'women',
 }
 
 export enum Size {
-  L = "L",
-  M = "M",
-  S = "S",
-  Xl = "XL",
-  Xs = "XS",
-  Xxl = "XXL",
+  L = 'L',
+  M = 'M',
+  S = 'S',
+  Xl = 'XL',
+  Xs = 'XS',
+  Xxl = 'XXL',
 }
-
